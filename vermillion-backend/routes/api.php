276b659c07642e218c4ca4ede4e98233c5276cf0
Viserys89 <fistvicky@gmail.com>
2026-06-07 14:44:50 +0700

    Tambah fitur Arsip Laporan HR dan topbar dinamis

--- vermillion-backend/routes/api.php
+++ vermillion-backend/routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\LeaveController;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\HostController;
 use App\Http\Controllers\FinanceController;
 
@@ -12,11 +12,17 @@ Route::get('/employees', [EmployeeController::class, 'index']); // Baca Data
 Route::post('/employees', [EmployeeController::class, 'store']); // Tambah Data
 Route::put('/employees/{id}', [EmployeeController::class, 'update']); // Edit Data
 Route::delete('/employees/{id}', [EmployeeController::class, 'destroy']); // Hapus Data
+
 // API Izin & Cuti
 Route::get('/leaves', [LeaveController::class, 'index']); // Dipakai HR
 Route::post('/leaves', [LeaveController::class, 'store']); // Dipakai Host/Karyawan
 Route::put('/leaves/{id}/status', [LeaveController::class, 'updateStatus']); // Dipakai HR
 Route::post('/login', [AuthController::class, 'login']);
+
+// API ARSIP LAPORAN
+Route::get('/reports', [ReportController::class, 'index']);
+Route::post('/reports', [ReportController::class, 'store']);
+Route::delete('/reports/{id}', [ReportController::class, 'destroy']);
 
 // API Host
 Route::get('/host/{user_id}/dashboard', [HostController::class, 'getDashboard']);
